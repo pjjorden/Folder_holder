9d19ce6b8f7a888dffc1e871f594e0e1cb963c36
pjjorden <p.jorden@commercecasino.net>
2017-10-12 16:20:35 -0700

    add new python file

---
+++ Greeter.py
@@ -0,0 +1,7 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("Welcome to the digital greeter!")
+Welcome to the digital greeter!
+>>> print ("How are you feeling today?")
+How are you feeling today?
+>>> feeling = input ()
